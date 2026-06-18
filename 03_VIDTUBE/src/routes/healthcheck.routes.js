c69d7de6744151954fcaa2083b93e18a49e056b1
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-06-18 23:02:25 +0530

    Delete 03_VIDTUBE directory

--- 03_VIDTUBE/src/routes/healthcheck.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { healthCheck } from "../controllers/healthcheck.controllers.js";
-
-const router = Router();
-router.route("/").get(healthCheck);
-
-export default router;
--- 03_VIDTUBE/src/utils/apiResponse.js
+++
@@ -1,9 +0,0 @@
-class ApiResponse {
-  constructor(statusCode, message, data = "success") {
-    this.statusCode = statusCode;
-    this.message = message;
-    this.data = data;
-    this.success = statusCode < 400;
-  }
-}
-export { ApiResponse };
